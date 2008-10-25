29f17e4bde4d1ca0097861e1798c328711840947
taro <higepon@gmail.com>
2008-10-25 09:29:14 +0000

    fasl psyntax

---
+++ scripts/scmc2fasl.scm
@@ -0,0 +1,26 @@
+(import (rnrs)
+        (srfi :1)
+        (mosh)
+        (mosh string))
+
+(define (extract obj)
+  (cond
+   [(vector? obj)
+    (vector-map extract obj)]
+   [(and (pair? obj) (eq? (car obj) '*insn*))
+    (make-instruction (second obj))]
+   [(and (pair? obj) (eq? (car obj) '*compiler-insn*))
+    (make-compiler-instruction (second obj))]
+   [(list? obj)
+    (map extract obj)]
+   [else obj]))
+
+(define (main args)
+  (with-input-from-file (second args)
+    (lambda ()
+      (call-with-port (open-file-output-port (third args))
+         (lambda (port)
+           (fasl-write port (extract (read)))))))
+  0)
+
+(main (command-line))
