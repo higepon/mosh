5f023cfc2c869313101a88485245a935745a1cf3
taro <higepon@gmail.com>
2008-11-28 06:38:23 +0000

    peephole optimization

--- scripts/bench2.scm
+++ scripts/bench2.scm
@@ -46,3 +46,37 @@
 ;; ./bench/clos.scm         :  0.61
 ;; ./bench/empty.scm        :  0.067
 ;; ./bench/load-library.scm :  0.109
+
+;; Darwin dorami.local 9.5.0 Darwin Kernel Version 9.5.0: Wed Sep  3 11:29:43 PDT 2008; root:xnu-1228.7.58~1/RELEASE_I386 i386
+;; Path: .
+;; URL: https://mosh-scheme.googlecode.com/svn/trunk
+;; Repository Root: https://mosh-scheme.googlecode.com/svn
+;; Repository UUID: 3dc39933-274d-0410-bf6f-2fa8959f7829
+;; Revision: 741
+;; Node Kind: directory
+;; Schedule: normal
+;; Last Changed Author: higepon
+;; Last Changed Rev: 741
+;; Last Changed Date: 2008-11-27 23:49:22 +0900 (Thu, 27 Nov 2008)
+
+;; ./bench/clos.scm         :  0.668
+;; ./bench/empty.scm        :  0.071
+;; ./bench/load-library.scm :  0.117
+
+;; Peephole optimization
+
+;; Darwin dorami.local 9.5.0 Darwin Kernel Version 9.5.0: Wed Sep  3 11:29:43 PDT 2008; root:xnu-1228.7.58~1/RELEASE_I386 i386
+;; Path: .
+;; URL: https://mosh-scheme.googlecode.com/svn/trunk
+;; Repository Root: https://mosh-scheme.googlecode.com/svn
+;; Repository UUID: 3dc39933-274d-0410-bf6f-2fa8959f7829
+;; Revision: 742
+;; Node Kind: directory
+;; Schedule: normal
+;; Last Changed Author: higepon
+;; Last Changed Rev: 742
+;; Last Changed Date: 2008-11-28 15:33:31 +0900 (Fri, 28 Nov 2008)
+
+;; ./bench/clos.scm         :  0.669
+;; ./bench/empty.scm        :  0.071
+;; ./bench/load-library.scm :  0.117
