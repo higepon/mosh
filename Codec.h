ddeadd3c53b1c60a2830b1bcd8acc9e6e36c09d5
kokosabu <kokosabu>
2009-02-04 12:04:11 +0000

    mini fixed

--- Codec.h
+++ Codec.h
@@ -47,10 +47,7 @@ public:
     virtual int out(uint8_t* buf, ucs4char c) = 0;
     virtual ucs4char in(BinaryInputPort* port) = 0;
     virtual ucs4string readWholeString(BinaryInputPort* port) = 0;
-    virtual ucs4string getCodecName() const
-    {
-        return UC("");
-    }
+    virtual ucs4string getCodecName() const = 0;
 };
 
 }; // namespace scheme
