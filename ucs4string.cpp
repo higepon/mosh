158ee17f33b73f534773d4273a93ccecb7938d94
taro <higepon@gmail.com>
2009-03-23 14:20:36 +0000

    win/win.cpp: UC moved to ucs4string.cpp for Mingw.

--- ucs4string.cpp
+++ ucs4string.cpp
@@ -32,6 +32,34 @@
 #include "scheme.h"
 #include "ucs4string.h"
 
+#ifdef _WIN32
+#include <map>
+#include <list>
+#include <vector>
+const ucs4char* UC(const char *str)
+{
+        typedef std::map<const char *, ucs4char*> Hash;
+        typedef std::list<std::vector<ucs4char> > Data;
+        static Hash hash;
+        static Data data;
+        Hash::iterator i = hash.find(str);
+        if (i == hash.end()) {
+                size_t len = strlen(str);
+                data.push_back(std::vector<ucs4char>());
+                std::vector<ucs4char>& d = data.back();
+                d.resize(len + 1);
+                for (size_t i = 0; i < len; i++) {
+                        d[i] = str[i];
+                }
+                d[len] = 0;
+                hash[str] = &d[0];
+                return &d[0];
+        } else {
+                return i->second;
+        }
+}
+#endif
+
 using namespace scheme;
 
 ucs4char* ucs4string::strdup()
