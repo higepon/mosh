86373f64e139df4029840629f5c5c35a97057d9c
taro <higepon@gmail.com>
2009-03-24 14:47:22 +0000

    test.bat

--- test/test.bat
+++ test/test.bat
@@ -1,9 +1,9 @@
-@echo off
+echo off
 setlocal
-cd ..
 if not exist mosh.exe goto notfound
 echo ----------------------------------------
-mosh.exe test/shell.scm
+echo input-port.scm
+mosh.exe test/input-port.scm
 if errorlevel 1 goto end
 echo Passed all tests
 goto end
