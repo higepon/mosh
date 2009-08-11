210051916272f0f097a4c973bc954ea04c0ccd11
taro <higepon@gmail.com>
2009-08-11 02:52:15 +0000

    test/mysql.scm: Fixe test/mysql.ss (Issue 85).

--- test/mysql.scm
+++ test/mysql.scm
@@ -1,114 +1,106 @@
 (import (rnrs)
         (mosh)
+        (srfi :2)
         (mosh mysql)
         (mosh ffi)
         (mosh test))
 
-(define mysql (guard (c (#t #f)) (mysql-init)))
-;; (guard (con
-;;         ((violation? con)
-;;          (display "no test because mysql connect failed\n")))
-(when mysql
+(and-let* ([mysql (guard (c (#t #f)) (mysql-init))]
+           [(not (pointer-null? (mysql-real-connect mysql "127.0.0.1" "root" "root" "mysql" 3306 NULL 0)))])
   (let ()
-(define (test/mysql-result result)
-  (when (pointer-null? result)
-    (assertion-violation 'mysql-store-result "failed"))
-  (test-true (pointer? (mysql-fetch-lengths result)))
-  (test-true (integer? (mysql-field-count mysql)))
-  (test-true (integer? (mysql-num-fields result)))
-  (test-true (integer? (mysql-row-seek result 0)))
-  (test-true (integer? (mysql-row-tell result)))
-  (let loop ([row (mysql-fetch-row result)])
-    (cond
-     [(pointer-null? row) '()]
-     [else
-;      (test-true (string? (mysql-row-ref row 0)))
-      (loop (mysql-fetch-row result))]))
-  (mysql-free-result result))
+    (define (test/mysql-result result)
+      (when (pointer-null? result)
+        (assertion-violation 'mysql-store-result "failed"))
+      (test-true (pointer? (mysql-fetch-lengths result)))
+      (test-true (integer? (mysql-field-count mysql)))
+      (test-true (integer? (mysql-num-fields result)))
+      (test-true (integer? (mysql-row-seek result 0)))
+      (test-true (integer? (mysql-row-tell result)))
+      (let loop ([row (mysql-fetch-row result)])
+        (cond
+         [(pointer-null? row) '()]
+         [else
+          (loop (mysql-fetch-row result))]))
+      (mysql-free-result result))
+    (unless (pointer-null? (mysql-query mysql "select Host, User from user"))
+      (assertion-violation 'mysql-query "failed"))
 
-(when (pointer-null? (mysql-real-connect mysql "127.0.0.1" "root" "root" "mysql" 3306 NULL 0))
-  (assertion-violation 'mysql-real-connect "failed"))
+    (test-true (integer? (mysql-set-character-set mysql "utf8")))
 
-(unless (pointer-null? (mysql-query mysql "select Host, User from user"))
-  (assertion-violation 'mysql-query "failed"))
+    (test-true (integer? (mysql-select-db mysql "mysql")))
+    (let* ([result (mysql-store-result mysql)])
+      (when (pointer-null? result)
+        (assertion-violation 'mysql-store-result "failed"))
+      (mysql-num-rows result)
+      (test-true (pointer? (mysql-fetch-lengths result)))
+      (test-true (integer? (mysql-field-count mysql)))
+      (test-true (integer? (mysql-set-server-option mysql 0)))
+      (let loop ([row (mysql-fetch-row result)])
+        (cond
+         [(pointer-null? row) '()]
+         [else
+                                        ;      (mysql-row-ref row 0)
+          (loop (mysql-fetch-row result))]))
 
-(test-true (integer? (mysql-set-character-set mysql "utf8")))
+      (let loop ([field (mysql-fetch-field result)])
+        (cond
+         [(pointer-null? field) '()]
+         [else
+          (test-true (string? (mysql-field-name field)))
+          (loop (mysql-fetch-field result))]))
+      (test-true (string? (mysql-field-name (mysql-fetch-field-direct result 1))))
+      (test-true (string? (mysql-error mysql)))
+      (mysql-data-seek result 1)
+      (test-true (integer? (mysql-errno mysql)))
+      (test-true (zero? (mysql-dump-debug-info mysql)))
+      (test-true (string? (mysql-get-client-info)))
+      (test-true (number? (mysql-affected-rows mysql)))
+      (test-true (zero? (mysql-autocommit mysql 1)))
+      (mysql-free-result result)
+      (test-true (zero? (mysql-change-user mysql "root" "root" "information_schema")))
+      (test-true (string? (mysql-character-set-name mysql)))
+      (test-true (zero? (mysql-commit mysql)))
+      (test-true (integer? (mysql-get-client-version)))
+      (test-true (string? (mysql-get-host-info mysql)))
+      (test-true (integer? (mysql-get-proto-info mysql)))
+      (test-true (string? (mysql-get-server-info mysql)))
+      (test-true (integer? (mysql-get-server-version mysql)))
+      (test-true (pointer-null? (mysql-get-ssl-cipher mysql)))
+      (let* ([from "abcd"]
+             [len (string-length from)]
+             [to-bv (make-bytevector (+ (* len 2) 1))])
+        (mysql-hex-string to-bv from len)
+        (test-true (equal? "61626364" (utf8->string to-bv))))
+      (test-true (or (string? (mysql-info mysql)) (pointer? (mysql-info mysql))))
+      (test-true (pointer? (mysql-insert-id mysql)))
 
-(test-true (integer? (mysql-select-db mysql "mysql")))
-(let* ([result (mysql-store-result mysql)])
-  (when (pointer-null? result)
-    (assertion-violation 'mysql-store-result "failed"))
-  (mysql-num-rows result)
-  (test-true (pointer? (mysql-fetch-lengths result)))
-  (test-true (integer? (mysql-field-count mysql)))
-  (test-true (integer? (mysql-set-server-option mysql 0)))
-  (let loop ([row (mysql-fetch-row result)])
-    (cond
-     [(pointer-null? row) '()]
-     [else
-;      (mysql-row-ref row 0)
-      (loop (mysql-fetch-row result))]))
+      (test/mysql-result (mysql-list-dbs mysql NULL))
+      (test/mysql-result (mysql-list-processes mysql))
+      (test/mysql-result (mysql-list-tables mysql NULL))
 
-  (let loop ([field (mysql-fetch-field result)])
-    (cond
-     [(pointer-null? field) '()]
-     [else
-      (test-true (string? (mysql-field-name field)))
-      (loop (mysql-fetch-field result))]))
-  (test-true (string? (mysql-field-name (mysql-fetch-field-direct result 1))))
-  (test-true (string? (mysql-error mysql)))
-  (mysql-data-seek result 1)
-  (test-true (integer? (mysql-errno mysql)))
-  (test-true (zero? (mysql-dump-debug-info mysql)))
-  (test-true (string? (mysql-get-client-info)))
-  (test-true (number? (mysql-affected-rows mysql)))
-  (test-true (zero? (mysql-autocommit mysql 1)))
-  (mysql-free-result result)
-  (test-true (zero? (mysql-change-user mysql "root" "root" "information_schema")))
-  (test-true (string? (mysql-character-set-name mysql)))
-  (test-true (zero? (mysql-commit mysql)))
-  (test-true (integer? (mysql-get-client-version)))
-  (test-true (string? (mysql-get-host-info mysql)))
-  (test-true (integer? (mysql-get-proto-info mysql)))
-  (test-true (string? (mysql-get-server-info mysql)))
-  (test-true (integer? (mysql-get-server-version mysql)))
-  (test-true (pointer-null? (mysql-get-ssl-cipher mysql)))
-  (let* ([from "abcd"]
-         [len (string-length from)]
-         [to-bv (make-bytevector (+ (* len 2) 1))])
-    (mysql-hex-string to-bv from len)
-    (test-true (equal? "61626364" (utf8->string to-bv))))
-  (test-true (or (string? (mysql-info mysql)) (pointer? (mysql-info mysql))))
-  (test-true (pointer? (mysql-insert-id mysql)))
-
-  (test/mysql-result (mysql-list-dbs mysql NULL))
-  (test/mysql-result (mysql-list-processes mysql))
-  (test/mysql-result (mysql-list-tables mysql NULL))
-
-  (test-true (zero? (mysql-more-results mysql)))
-  (test-true (integer? (mysql-next-result mysql)))
-  (test-true (zero? (mysql-options mysql 2 NULL)))
-  (test-true (zero? (mysql-ping mysql)))
-  (let* ([from "hoge"]
-         [len (string-length from)]
-         [to-bv (make-bytevector (+ (* len 2) 1))])
-    (mysql-real-escape-string mysql to-bv from len)
-    (test-true (string=? "hoge" (utf8->string to-bv))))
-  (test-true (integer? (mysql-real-query mysql "select Host, User from user" 28)))
-  (test-true (integer? (mysql-refresh mysql 0)))
-  (mysql-reload mysql)
-  (test-true (integer? (mysql-rollback mysql)))
-  ;; This works, but don't do this in test.
-  ;; (mysql-shutdwon mysql NULL)
-  (test-true (string? (mysql-sqlstate mysql)))
-  (test-true (zero? (mysql-ssl-set mysql "" "" "" "")))
-  (test-true (string? (mysql-stat mysql)))
-  (test-true (integer? (mysql-thread-id mysql)))
-  (test-true (pointer-null? (mysql-use-result mysql)))
-  (test-true (integer? (mysql-warning-count mysql)))
-  (mysql-close mysql)
-;  (mysql-library-end)
-  '()))
+      (test-true (zero? (mysql-more-results mysql)))
+      (test-true (integer? (mysql-next-result mysql)))
+      (test-true (zero? (mysql-options mysql 2 NULL)))
+      (test-true (zero? (mysql-ping mysql)))
+      (let* ([from "hoge"]
+             [len (string-length from)]
+             [to-bv (make-bytevector (+ (* len 2) 1))])
+        (mysql-real-escape-string mysql to-bv from len)
+        (test-true (string=? "hoge" (utf8->string to-bv))))
+      (test-true (integer? (mysql-real-query mysql "select Host, User from user" 28)))
+      (test-true (integer? (mysql-refresh mysql 0)))
+      (mysql-reload mysql)
+      (test-true (integer? (mysql-rollback mysql)))
+      ;; This works, but don't do this in test.
+      ;; (mysql-shutdwon mysql NULL)
+      (test-true (string? (mysql-sqlstate mysql)))
+      (test-true (zero? (mysql-ssl-set mysql "" "" "" "")))
+      (test-true (string? (mysql-stat mysql)))
+      (test-true (integer? (mysql-thread-id mysql)))
+      (test-true (pointer-null? (mysql-use-result mysql)))
+      (test-true (integer? (mysql-warning-count mysql)))
+      (mysql-close mysql)
+                                        ;  (mysql-library-end)
+      '())))
 
 (test-results)
-)
