d025ff42e6282d680e1db2288b814dd98b0ba28d
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-06-27 00:11:06 +0900

    Checked a return value of syscall.

--- src/scheme.cpp
+++ src/scheme.cpp
@@ -120,8 +120,9 @@ void mosh_init()
 #ifdef MONA
     const char* MAP_FILE_PATH = "/APPS/MOSH.MAP";
     uint32_t pid = syscall_get_pid();
-    if (!syscall_stack_trace_enable(pid, MAP_FILE_PATH)) {
-        fprintf(stderr, "map file not found\n");
+    intptr_t ret = syscall_stack_trace_enable(pid, MAP_FILE_PATH);
+    if (ret != M_OK) {
+        fprintf(stderr, "syscall_stack_trace_enable error %d\n", ret);
         exit(-1);
     }
 #endif // MONA
