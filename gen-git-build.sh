94b1943c32edd2b4e22d773b456cf06e32f06c84
okuoku <mjt@cltn.org>
2010-08-14 00:21:38 +0900

    Generate.mk: created

--- gen-git-build.sh
+++ gen-git-build.sh
@@ -2,4 +2,5 @@
 MYMAKE=`which gmake 2>/dev/null 1>/dev/null && echo -n gmake || echo -n make`
 autoreconf -ifv && \
 $MYMAKE -C boot bootstrap && \
-$MYMAKE -C boot
+$MYMAKE -C boot && \
+$MYMAKE -f Generate.mk top_srcdir=.  src/Instruction.h src/NumberReader.tab.cpp src/NumberReader.tab.hpp src/NumberScanner.cpp src/OSConstants.h src/Object-accessors.h src/Reader.tab.cpp src/Reader.tab.hpp src/Scanner.cpp src/cprocedures.cpp src/labels.cpp
