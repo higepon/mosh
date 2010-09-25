02542d8c5e7a7a3ba0a0f813e97dda508409f67c
okuoku <mjt@cltn.org>
2010-09-26 01:35:52 +0900

    no longer needs _XOPEN_SOURCE at MacOS X

--- misc/dist/mac/run-configure.mk
+++ misc/dist/mac/run-configure.mk
@@ -4,11 +4,11 @@ all: d32 d64
 d32:
 	-rm -rf darwin32
 	mkdir darwin32
-	(cd darwin32 && CFLAGS="-arch i386 -D_XOPEN_SOURCE $(CFLAGS)" CXXFLAGS="-arch i386 $(CXXFLAGS)" ../$(MOSHCONFIG))
+	(cd darwin32 && CFLAGS="-arch i386 $(CFLAGS)" CXXFLAGS="-arch i386 $(CXXFLAGS)" ../$(MOSHCONFIG))
 
 d64:
 	-rm -rf darwin64
 	mkdir darwin64
-	(cd darwin64 && CFLAGS="-arch x86_64 -D_XOPEN_SOURCE $(CFLAGS)" CXXFLAGS="-arch x86_64 $(CXXFLAGS)" ../$(MOSHCONFIG))
+	(cd darwin64 && CFLAGS="-arch x86_64 $(CFLAGS)" CXXFLAGS="-arch x86_64 $(CXXFLAGS)" ../$(MOSHCONFIG))
 
 
