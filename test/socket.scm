b59b78b3efbdfed6df54f22de891aa343e34fc02
Higepon (MINOWA Taro) <higepon@gmail.com>
2011-01-11 21:51:09 +0900

    Adde test for http library.

--- test/socket.scm
+++ test/socket.scm
@@ -48,11 +48,4 @@
 ;;       (test-true (string-contains s "HTTP/1.1")))
 ;;     ))
 
-(when (ssl-supported?)
-  (test-equal "19292868552" (cdr (vector-ref (json-read (open-string-input-port (http-get "https://graph.facebook.com/19292868552"))) 0)))
-  )
-
-(test-true (string-contains (http-get "http://www.hatena.ne.jp") "</html>"))
-(test-true (string-contains (http-get "http://www.hatena.ne.jp:80") "</html>"))
-(test-true (string-contains (http-get "http://www.hatena.ne.jp:80/") "</html>"))
 (test-results)
