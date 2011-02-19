d87813ba773abb9855f5001f2c549635c91564b8
okuoku <mjt@cltn.org>
2011-02-19 23:44:45 +0900

    make sure test/read-only.txt is read-only.
    git don't preserve these attributes.

--- gen-git-build.sh
+++ gen-git-build.sh
@@ -8,3 +8,4 @@ autoreconf -ifv && \
 $MYMAKE -C boot bootstrap && \
 $MYMAKE -C boot && \
 $MYMAKE -f Generate.mk top_srcdir=.  src/Instruction.h src/NumberReader.tab.cpp src/NumberReader.tab.hpp src/NumberScanner.cpp src/OSConstants.h src/Object-accessors.h src/Reader.tab.cpp src/Reader.tab.hpp src/Scanner.cpp src/cprocedures.cpp src/labels.cpp src/all-tests.scm
+chmod -w test/read-only.txt
