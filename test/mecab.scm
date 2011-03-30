1145b355f4eaeae4982d46c23b96171526893863
Taro Minowa(Higepon) <higepon@users.sourceforge.jp>
2011-03-30 16:47:35 +0900

    (Red) Added test for mecab-node-feature.

--- test/mecab.scm
+++ test/mecab.scm
@@ -11,13 +11,16 @@
     (test-equal "僕\t名詞,代名詞,一般,*,*,*,僕,ボク,ボク\nは\t助詞,係助詞,*,*,*,*,は,ハ,ワ\nお腹\t名詞,一般,*,*,*,*,お腹,オナカ,オナカ\nが\t助詞,格助詞,一般,*,*,*,が,ガ,ガ\nすい\t動詞,自立,*,*,五段・カ行イ音便,連用タ接続,すく,スイ,スイ\nた\t助動詞,*,*,*,特殊・タ,基本形,た,タ,タ\nEOS\n"
                 (mecab-sparse-tostr2 m text len))
     (let loop ([node (mecab-sparse-tonode2 m text len)]
-               [ret '()])
+               [surface* '()]
+               [feature* '()])
       (cond
        [(pointer-null? node)
-        (test-equal '("" "僕" "は" "お腹" "が" "すい" "た" "") (reverse ret))]
+        (test-equal '("" "僕" "は" "お腹" "が" "すい" "た" "") (reverse surface*))
+        (test-equal '() (reverse feature*))]
        [else
         (loop (mecab-node-next node)
-              (cons (mecab-node-surface node) ret))]))
+              (cons (mecab-node-surface node) surface*)
+              (cons (mecab-node-feature node) feature*))]))
     (test-equal '("僕" "は" "お腹" "が" "すい" "た") (mecab-node-surface* (mecab-sparse-tonode2 m text len)))
     (mecab-destroy m)
 ))
