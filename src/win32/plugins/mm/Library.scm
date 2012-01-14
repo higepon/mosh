52fa886a7035e098303c71b00f6a4b7b2c8fa9f4
okuoku <mjt@cltn.org>
2012-01-14 23:33:21 +0900

    Win32: Add Joystick read support

---
+++ src/win32/plugins/mm/Library.scm
@@ -0,0 +1,9 @@
+(mosh-winmm
+  c-function-table
+  *internal*
+  (plugin: mosh_winmm)
+  (libname: mosh-winmm)
+  #(ret name args)
+  (int mmm_joy_count)
+  (int mmm_joy_read (int void*))
+  )
---
+++ src/win32/plugins/mm/moshmm.c
@@ -0,0 +1,25 @@
+#include "config.h"
+
+#include <windows.h>
+#include <mmsystem.h>
+
+MOSHEXPORT
+int /* count */
+mmm_joy_count(void){
+    return joyGetNumDevs();
+}
+
+MOSHEXPORT
+int /* bool */
+mmm_joy_read(int id,void* p){
+    int res;
+    JOYINFOEX* joy = (JOYINFOEX *)p;
+    joy->dwSize = sizeof(JOYINFOEX);
+
+    res = joyGetPosEx(id,joy);
+    if(res != JOYERR_NOERROR){
+        return 0;
+    }else{
+        return 1;
+    }
+}
