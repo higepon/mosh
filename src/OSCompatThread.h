e1d734ad633e2aae2426d612ec1c93459d7a9aa5
Taro Minowa <higepon@gmail.com>
2022-09-04 04:28:58 +0000

    Add explicit to ConditionVariable

--- src/OSCompatThread.h
+++ src/OSCompatThread.h
@@ -368,7 +368,7 @@ namespace scheme {
             initialize();
         }
 
-        ConditionVariable(const ucs4string& name) : name_(name)
+        explicit ConditionVariable(const ucs4string& name) : name_(name)
         {
             initialize();
         }
