0593fb1c10727a97157f25bd7ba0ed0efb1eeed8
Taro Minowa <higepon@gmail.com>
2022-09-06 22:50:11 +0000

    Save psyntax.scm

--- gen-git-build.sh
+++ gen-git-build.sh
@@ -10,7 +10,6 @@ mosh --loadpath=lib misc/scripts/gen-nmosh-bootstrap.sps
 mosh --loadpath=lib misc/scripts/gen-corelibmk.sps
 MYMAKE=`which gmake 2>/dev/null 1>/dev/null && echo gmake || echo make`
 autoreconf -ifv && \
-$MYMAKE -C boot bootstrap && \
 $MYMAKE -C boot/runtimes/psyntax-mosh && \
 $MYMAKE -C boot && \
 $MYMAKE -f Generate.mk top_srcdir=.  src/Instruction.h src/NumberReader.tab.cpp src/NumberReader.tab.hpp src/NumberScanner.cpp src/OSConstants.h src/Object-accessors.h src/Reader.tab.cpp src/Reader.tab.hpp src/Scanner.cpp src/cprocedures.cpp src/labels.cpp src/all-tests.scm
