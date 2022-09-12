7cfbc71e4be3328a8d17f218dc5eb6080dde7576
Taro Minowa <higepon@gmail.com>
2022-09-12 08:39:56 +0000

    add define-macro implemented in define-syntax

--- boot/vm.scm
+++ boot/vm.scm
@@ -1,6 +1,19 @@
 (import (rnrs)
         (only (mosh) include))
 
+ (define-syntax define-macro
+     (lambda (x)
+       (syntax-case x ()
+         [(_ (name . params) body1 body2 ...)
+          #'(define-macro name (lambda params body1 body2 ...))]
+         [(_ name expander)
+          #'(define-syntax name
+              (lambda (y)
+                (syntax-case y ()
+                  ((k . args)
+                   (let ((lst (syntax->datum #'args)))
+                     (datum->syntax #'k (apply expander lst)))))))])))        
+
 (set! debug-print-width 3000)
 
 (define alist-cons acons)
