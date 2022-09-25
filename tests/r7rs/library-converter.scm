e1b638420bb6ff17cb61348de802c0b7f61c19b2
Taro Minowa <higepon@gmail.com>
2022-09-25 06:04:32 +0000

    wip

--- tests/r7rs/library-converter.scm
+++ tests/r7rs/library-converter.scm
@@ -7,12 +7,13 @@
   (match exp
     [('define-library (name* ...)
                       ('export export* ...)
+                      ('import import* ...)
     )
-        (values #t name* export*)]
+        (values #t name* export* import*)]
     [else (values #f #f)]))
 
 (test-values (values #t '(my lib)  '(make rows (rename put! set!)))
-  (parse-define-library '(define-library (my lib) (export make rows (rename put! set!)))))
+  (parse-define-library '(define-library (my lib) (export make rows (rename put! set!) (import (scheme base))))))
 
 (test-results)
 
