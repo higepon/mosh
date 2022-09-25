7d4f41a7ece3681102968c06e7229efbb408bf23
Taro Minowa <higepon@gmail.com>
2022-09-25 06:43:35 +0000

    wip

--- tests/r7rs/library-converter.scm
+++ tests/r7rs/library-converter.scm
@@ -27,19 +27,23 @@
 (define parse-library-body
   (case-lambda
    [(body*)
-    (parse-library-body body* '())]
-   [(body* include*)
+    (parse-library-body body* '() '())]
+   [(body* include* begin*)
      (match body*
        [(('include file) other* ...)
-          (parse-library-body other* (cons file include*))]
-       [else (values (reverse include*))])]))
+          (parse-library-body other* (cons file include*) begin*)]
+       [(('begin bbody* ...) other* ...)
+          (parse-library-body other* include* (cons bbody* begin*))]
+       [else (values (reverse include*) (reverse begin*))])]))
 
-(test-values (values '("my_lib.scm"))
+(test-values (values '("my_lib.scm") '())
   (parse-library-body '((include "my_lib.scm"))))
 
-(test-values (values '("my_lib1.scm" "my_lib2.scm"))
+(test-values (values '("my_lib1.scm" "my_lib2.scm") '())
   (parse-library-body '((include "my_lib1.scm") (include "my_lib2.scm"))))
 
+(test-values (values '("my_lib1.scm" "my_lib2.scm") '(((a) 1 2)))
+  (parse-library-body '((include "my_lib1.scm") (include "my_lib2.scm") (begin (a) 1 2))))
 
 (test-results)
 
