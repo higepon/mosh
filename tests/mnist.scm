3ae181e5ee01514f56bd14c893e8eff41d274c59
Taro Minowa <higepon@gmail.com>
2022-10-05 11:04:54 +0000

    Implemented forward path

--- tests/mnist.scm
+++ tests/mnist.scm
@@ -26,10 +26,7 @@
               ((= i m) v)
             (vec-at v i (make-vector n value))))]
       [(m n)
-        (let ([v (make-vector m)])
-          (do ((i 0 (+ i 1)))
-              ((= i m) v)
-            (vec-at v i (make-vector n))))]))
+        (make-vector* m n 0)]))
 
 ;; Short version of vector-set! and vector-ref.
 (define-syntax vec-at
@@ -120,16 +117,31 @@
               (mat-at mat i j (mul i j))))))
 
 (define (matrix-add a b)
-    (unless (equal? (matrix-shape a) (matrix-shape b))
-        (error "matrix-add shapes don't match" (matrix-shape a) (matrix-shape b)))
-    (let ([mat (matrix-zeros-like a)]
-          [nrows (vec-at (matrix-shape a) 0)]
-          [ncols (vec-at (matrix-shape a) 1)])
-      (do ((i 0 (+ i 1)))
-          ((= i nrows) mat)
-            (do ((j 0 (+ j 1)))
-              ((= j ncols))
-              (mat-at mat i j (+ (mat-at a i j) (mat-at b i j)))))))
+  (cond
+    [(equal? (matrix-shape a) (matrix-shape b))
+        (let ([mat (matrix-zeros-like a)]
+            [nrows (vec-at (matrix-shape a) 0)]
+            [ncols (vec-at (matrix-shape a) 1)])
+        (do ((i 0 (+ i 1)))
+            ((= i nrows) mat)
+                (do ((j 0 (+ j 1)))
+                ((= j ncols))
+                (mat-at mat i j (+ (mat-at a i j) (mat-at b i j))))))]
+     ;; broadcast case.
+     [(= (vec-at (matrix-shape a) 0) 1)
+       (error "not implemented yet :)")]
+     ;; broadcast case.
+     [(= (vec-at (matrix-shape b) 0) 1)
+        (let ([mat (matrix-zeros-like a)]
+            [nrows (vec-at (matrix-shape a) 0)]
+            [ncols (vec-at (matrix-shape a) 1)])
+        (do ((i 0 (+ i 1)))
+            ((= i nrows) mat)
+                (do ((j 0 (+ j 1)))
+                ((= j ncols))
+                (mat-at mat i j (+ (mat-at a i j) (mat-at b 0 j))))))]
+     [else
+        (error "matrix-add shapes don't match" (matrix-shape a) (matrix-shape b))]))
 
 (define (matrix-sum a)
     (let* ([lst (matrix->list* a)]
@@ -151,13 +163,14 @@
 
 ;; softmax.
 (define (softmax a)
-    (unless (= 1 (vec-at (matrix-shape a) 0))
-        (error "softmax requires shape of (1 N)" (matrix-shape a)))
     (let* ([c (matrix-max a)]
            [a (matrix-map (lambda (e) (- e c)) a)]
-           [mat-exp (matrix-map (lambda (e) (exp e)) a)]
-           [sum-exp (matrix-sum  mat-exp)])
-      (matrix-map (lambda (e) (/ e sum-exp)) mat-exp)))
+           [mat-exp (matrix-map (lambda (e) (exp e)) a)])
+      (vector-map
+        (lambda (row)
+          (let ([row-sum (sum (vector->list row))])
+            (vector-map (lambda (e) (/ e row-sum)) row)))
+        mat-exp)))
 
 ;; Matrix shape.
 (test-equal '#(1 2) (matrix-shape (matrix ((1 2)))))
@@ -194,10 +207,16 @@
    (test-equal (matrix ((14) (32)))
                (matrix-mul a b)))
 
-;; Matrix additions.
+;; Matrix addition.
 (let ([a (matrix ((1 2) (3 4)))]
       [b (matrix ((5 6) (7 8)))])
    (test-equal (matrix ((6 8) (10 12)))
+               (matrix-add a b)))
+
+;; Matrix addition broadcast.
+(let ([a (matrix ((1 2) (3 4)))]
+      [b (matrix ((5 6)))])
+   (test-equal (matrix ((6 8) (8 10)))
                (matrix-add a b)))
 
 ;; matrix-map
@@ -216,11 +235,19 @@
                   (good-enough? 0.24519181 (mat-at mat 0 1))
                   (good-enough? 0.73659691 (mat-at mat 0 2)))))
 
+(test-true (let ([mat (softmax (matrix ((0.3 2.9 4.0) (0.3 2.9 4.0))))])
+             (and (good-enough? 0.01821127 (mat-at mat 0 0))
+                  (good-enough? 0.24519181 (mat-at mat 0 1))
+                  (good-enough? 0.73659691 (mat-at mat 0 2))
+                  (good-enough? 0.01821127 (mat-at mat 1 0))
+                  (good-enough? 0.24519181 (mat-at mat 1 1))
+                  (good-enough? 0.73659691 (mat-at mat 1 2)))))
+
 ;; Read training images
 (define (load-train)
   (call-with-port (open-binary-input-file "/workspace/train-images-idx3-ubyte")
     (lambda (port)
-      (let ([num-images 600] ;; TODO 60000
+      (let ([num-images 60] ;; TODO 60000
             [image-width 28]
             [image-height 28])
         ;; Skip the header
@@ -228,6 +255,22 @@
         ;; Read images as bytevector
         (bytevector->matrix (read-bytevector (* num-images image-width image-height) port) num-images)))))
 
-(test-equal #(600 784) (matrix-shape (load-train)))
+(test-equal #(6 784) (matrix-shape (load-train)))
+
+(define w1 (matrix 784 50))
+(define b1 (matrix 1 50))
+(define w2 (matrix 50 100))
+(define b2 (matrix 1 100))
+(define w3 (matrix 100 10))
+(define b3 (matrix 1 10))
+
+(let* ([x (load-train)]
+       [a1 (matrix-add (matrix-mul x w1) b1)]
+       [z1 (sigmoid a1)]
+       [a2 (matrix-add (matrix-mul z1 w2) b2)]
+       [z2 (sigmoid a2)]
+       [a3 (matrix-add (matrix-mul z2 w3) b3)]
+       [y (softmax a3)])
+  (display y))
 
 (test-results)
