42e72b193694fd27b27d08c67878dce324a476a6
Taro Minowa <higepon@gmail.com>
2022-10-07 08:24:53 +0000

    Add (mosh chibi test)

--- tests/r7rs/r7rs-tests.scm
+++ tests/r7rs/r7rs-tests.scm
@@ -6,72 +6,8 @@
         (scheme eval) (scheme process-context) (scheme case-lambda)
         (except (mosh test) test-error)
         (only (rnrs r5rs (6)) null-environment)
+        (mosh chibi test)
         )
-
-;; Thin layer to make this work for (mosh test).
-(define (test-begin . args) #f)
-
-(define (test-end . args) #f)
-
-(define-syntax test
-  (syntax-rules ()
-    ((_ expected expr)
-      (if (number? expected)
-        (test-true (number-equal? expected expr))
-        (test-equal expected expr)))
-    ((_ name expected expr)
-      (test-equal expected expr))))
-
-(define-syntax test-error
-  (syntax-rules ()
-    ((_ expr)
-      (test-true (guard (e (else #t))
-          (begin expr #f))))))
-
-(define-syntax test-assert
-  (syntax-rules ()
-    ((_ str expr)
-          (test-true expr))))
-
-(define current-test-epsilon (make-parameter 1e-5))
-
-(define (approx-equal? a b epsilon)
-  (cond
-   ((> (abs a) (abs b))
-    (approx-equal? b a epsilon))
-   ((zero? a)
-    (< (abs b) epsilon))
-   (else
-    (< (abs (/ (- a b) b)) epsilon))))
-
-;; From (chibi test)
-(define (test-equal? expect res)
-  (or (equal? expect res)
-      (if (real? expect)
-          (and (inexact? expect)
-               (real? res)
-               ;; tests which expect an inexact value can
-               ;; accept an equivalent exact value
-               ;; (inexact? res)
-               (approx-equal? expect res (current-test-epsilon)))
-          (and (complex? res)
-               (complex? expect)
-               (test-equal? (real-part expect) (real-part res))
-               (test-equal? (imag-part expect) (imag-part res))))))
-
-(define (number-equal? expect res)
-  (or (equal? expect res)
-      (if (real? expect)
-          (and (inexact? expect)
-               (real? res)
-               ;; tests which expect an inexact value can
-               ;; accept an equivalent exact value
-               ;; (inexact? res)
-               (approx-equal? expect res (current-test-epsilon)))
-          (and (complex? res)
-               (complex? expect)
-               (test-equal? (real-part expect) (real-part res))
-               (test-equal? (imag-part expect) (imag-part res))))))
 
 ;; R7RS test suite.  Covers all procedures and syntax in the small
 ;; language except `delete-file'.  Currently assumes full-unicode
