7005cc69f7a653ea4cab6e9ae64d83ae24e6ce24
Taro Minowa <higepon@gmail.com>
2022-11-24 23:29:00 +0000

    raw_pointer

--- rmosh/src/main.rs
+++ rmosh/src/main.rs
@@ -5,4 +5,11 @@ fn main() {
     println!("number={}", num);
 
     println!("number & 0x3={}", num & 0x3);
+
+    let address = 0xdeadbeefusize;
+    let raw_pointer = address as *const i32;
+    unsafe {
+        // This causes Segmentation fault.
+        println!("pointer={}", *raw_pointer);
+    }
 }
