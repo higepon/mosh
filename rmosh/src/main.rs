cde2d5e54e168202cf13a27995f75c83693971f1
Taro Minowa <higepon@gmail.com>
2022-11-24 23:38:00 +0000

    fixnum

--- rmosh/src/main.rs
+++ rmosh/src/main.rs
@@ -10,6 +10,21 @@ fn main() {
     let raw_pointer = address as *const i32;
     unsafe {
         // This causes Segmentation fault.
-        println!("pointer={}", *raw_pointer);
+        // println!("pointer={}", *raw_pointer);
     }
+
+    let num_tag_bits = 3;
+    let fixnum_tag = 0x1;
+    let fixnum: isize = 123456;
+    // Create fixnum.
+    let obj = fixnum << num_tag_bits;
+    let obj = obj | fixnum_tag;
+    let obj = obj as *const i64;
+
+    // print fixnum
+    let num = obj as isize >> num_tag_bits;
+    print!("num={}", num);
+
+
+    // refactor later.
 }
