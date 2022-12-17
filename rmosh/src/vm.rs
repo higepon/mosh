8985eb59b24d00a594bc42c8e3dda74e681c3290
Taro Minowa <higepon@gmail.com>
2022-12-17 08:44:01 +0000

    test54

--- rmosh/src/vm.rs
+++ rmosh/src/vm.rs
@@ -2028,4 +2028,27 @@ pub mod tests {
         test_ops_with_size(&mut vm, ops, Object::Number(7), 0);
     }
 
+    #[test]
+    fn test_test54() {
+        let mut vm = Vm::new();        
+        let ops = vec![
+            Op::LetFrame(3),
+            Op::Constant(Object::Number(3)),
+            Op::Push,
+            Op::Enter(1),
+            Op::LetFrame(2),
+            Op::ReferLocal(0),
+            Op::Push,
+            Op::Constant(Object::Number(1)),
+            Op::NumberAdd,
+            Op::Push,
+            Op::Enter(1),
+            Op::ReferLocal(0),
+            Op::Leave(1),
+            Op::Leave(1),
+            Op::Halt,
+        ];
+        test_ops_with_size(&mut vm, ops, Object::Number(4), 0);
+    }
+
 }
