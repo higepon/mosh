abd053c285665699e76927bcba308f34129e6b40
Taro Minowa <higepon@gmail.com>
2023-01-01 03:18:10 +0000

    write symbol

--- rmosh/scripts/fasl_writer.scm
+++ rmosh/scripts/fasl_writer.scm
@@ -3,7 +3,7 @@
 (import (match))
 (import (mosh control))
 (import (mosh test))
-(import (only (rnrs) bytevector-s64-native-set! bytevector-u32-native-set! open-bytevector-output-port put-u8 put-bytevector))
+(import (only (rnrs) bytevector-s64-native-set! bytevector-u32-native-set! bytevector-u16-native-set! open-bytevector-output-port put-u8 put-bytevector))
 (import (scheme base))
 (import (scheme case-lambda))
 (import (scheme write))
@@ -13,6 +13,7 @@
 (define TAG_FALSE  2)
 (define TAG_NIL    3)
 (define TAG_CHAR   4)
+(define TAG_SYMBOL 5)
 
 (define (put-s64 port n)
   (let1 bv (make-bytevector 8)
@@ -22,6 +23,11 @@
 (define (put-u32 port n)
   (let1 bv (make-bytevector 4)
      (bytevector-u32-native-set! bv 0 n)
+     (put-bytevector port bv)))
+
+(define (put-u16 port n)
+  (let1 bv (make-bytevector 2)
+     (bytevector-u16-native-set! bv 0 n)
      (put-bytevector port bv)))
 
 (define write-constant
@@ -34,6 +40,14 @@
         [(? fixnum? n)
           (put-u8 port TAG_FIXNUM)
           (put-s64 port n)]
+        [(? symbol? s)
+          (put-u8 port TAG_SYMBOL)
+          (let1 str (symbol->string s)
+            (put-u16 port (string-length str))
+            (for-each
+              (lambda (c)
+                (put-u32 port (char->integer c)))
+              (string->list str)))]
         [#t
           (put-u8 port TAG_TRUE)]
         [#f
@@ -51,10 +65,12 @@
 (display (write-constant #f))
 (display (write-constant '()))
 (display (write-constant #\a))
+(display (write-constant 'hello))
 
 (test-equal #vu8(0 3 0 0 0 0 0 0 0) (write-constant 3))
 (test-equal #vu8(1) (write-constant #t))
 (test-equal #vu8(2) (write-constant #f))
 (test-equal #vu8(3) (write-constant '()))
 (test-equal #vu8(4 97 0 0 0) (write-constant #\a))
+(test-equal #vu8(5 5 0 104 0 0 0 101 0 0 0 108 0 0 0 108 0 0 0 111 0 0 0) (write-constant 'hello))
 (test-results)
