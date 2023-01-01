beab0c41d641e2f8b39cb9e55b6fc7bc94e94fb2
Taro Minowa <higepon@gmail.com>
2023-01-01 01:22:11 +0000

    fasl writer

--- rmosh/scripts/fasl_writer.scm
+++ rmosh/scripts/fasl_writer.scm
@@ -9,20 +9,28 @@
 (import (scheme write))
 
 (define TAG_FIXNUM 0)
+(define TAG_TRUE   1)
 
 (define (put-s64 port n)
   (let1 bv (make-bytevector 8)
      (bytevector-s64-native-set! bv 0 n)
      (put-bytevector port bv)))
 
-(define (write-constant c port)
-  (match c
-    [(? fixnum? n)
-      (put-u8 port TAG_FIXNUM)
-      (put-s64 port n)]))
+(define write-constant
+  (case-lambda
+    [(c port)
+      (match c
+        [(? fixnum? n)
+          (put-u8 port TAG_FIXNUM)
+          (put-s64 port n)])]
+    [c
+      (let-values ([(p get) (open-bytevector-output-port)])
+        (write-constant 3 p)
+        (get))]))
 
-(let-values ([(p get) (open-bytevector-output-port)])
-  (write-constant 3 p)
-  (display (get)))
+
+  (display (write-constant 3))
+
+(test-equal #vu8(0 3 0 0 0 0 0 0 0) (write-constant 3))
 
 (test-results)
