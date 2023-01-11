18541b58115940cd62374df85957e44bd13204ae
Taro Minowa <higepon@gmail.com>
2023-01-11 08:09:08 +0000

    rename

--- rmosh/src/main.rs
+++ rmosh/src/main.rs
@@ -1,7 +1,7 @@
 extern crate num_derive;
 #[macro_use] extern crate lalrpop_util;
 
-lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
+lalrpop_mod!(pub reader); // synthesized by LALRPOP
 pub mod alloc;
 pub mod compiler;
 pub mod equal;
@@ -18,10 +18,10 @@ use crate::vm::Vm;
 
 
 #[test]
-fn calculator1() {
+fn datam_parser() {
     let mut vm = Vm::new();
-    println!("{:?}", calculator1::DatumParser::new().parse(&mut vm.gc, "#t"));
-    println!("{:?}", calculator1::DatumParser::new().parse(&mut vm.gc, "(#t)"));    
+    println!("{:?}", reader::DatumParser::new().parse(&mut vm.gc, "#t"));
+    println!("{:?}", reader::DatumParser::new().parse(&mut vm.gc, "(#t)"));    
     //assert!(calculator1::TermParser::new().parse("#t").is_ok());
     //assert!(calculator1::TermParser::new().parse("#f").is_ok());
     //assert!(calculator1::TermParser::new().parse("#T").is_ok());
