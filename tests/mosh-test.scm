4005f12b638831660f9f857b4a88962636a42b5f
Taro Minowa <higepon@gmail.com>
2023-04-11 09:11:01 +0000

    tidy

--- tests/mosh-test.scm
+++ tests/mosh-test.scm
@@ -43,34 +43,5 @@
 
 (mosh-test:test-write-equal "#\\b" #\a)
 
-(test-equal
-"============================================================
-  (test-true (number? #\\a))
-============================================================
 
-  ERROR : (test-true (car 3))
-    Condition components:
-     1. &assertion
-     2. &who             who: \"car\"
-     3. &message         message: \"pair required\"
-     4. &irritants       irritants: (3)
-
-============================================================
-  (+ 1 2) : expected 4, actual 3
-============================================================
-  FAILURE : my tests failed
-============================================================
-  3 :
-    expected to raise error which satisfies error? predicate
-============================================================
-  (car 3) :
-    raised error doesn't satisfy pair? predicate
-============================================================
-  3 : expected (), actual 3
-============================================================
-  (write #\\a) : expected #\\b, actual #\\a
-" (mosh-test:test-error-string))
-
-(test-equal
- "[  FAILED  ] 4 passed, 8 failed." (mosh-test:test-summary-string))
 (test-end)
