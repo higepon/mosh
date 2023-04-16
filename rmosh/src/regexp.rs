237668d2d26ec895e50d9ed9f4bdb2095d9b573d
Taro Minowa <higepon@gmail.com>
2023-04-16 22:53:29 +0000

    parse regexp

--- rmosh/src/regexp.rs
+++ rmosh/src/regexp.rs
@@ -1,10 +1,15 @@
-use crate::gc::{GcHeader, ObjectType, Trace};
+use crate::{
+    error::SchemeError,
+    gc::{GcHeader, ObjectType, Trace},
+};
+use onig::Regex;
 use std::fmt::{self, Debug, Display};
 
 #[repr(C)]
 pub struct Regexp {
     pub header: GcHeader,
     string: String,
+    regex: Regex,
 }
 
 impl Trace for Regexp {
@@ -12,11 +17,16 @@ impl Trace for Regexp {
 }
 
 impl Regexp {
-    pub fn new(s: &str) -> Self {
-        Self {
+    pub fn new(s: &str) -> Result<Self, SchemeError> {
+        let regex = Regex::new(s).map_err(|e| {
+            SchemeError::assertion_violation("lexer", &format!("regexp errpr {}", e), &[])
+        })?;
+
+        Ok(Self {
             header: GcHeader::new(ObjectType::Regexp),
             string: s.to_string(),
-        }
+            regex: regex,
+        })
     }
 }
 
